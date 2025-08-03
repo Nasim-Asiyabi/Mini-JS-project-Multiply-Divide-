2bb2c749e58f7db26c67f4bd1e9e4ea94dc70884
Nasim <nasimasiyabii@gmail.com>
2025-08-03 23:58:02 +0330

    js file

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function multiply(){
+    num1 = document.getElementById("firstNumber").value ; 
+    num2 = document.getElementById("secondNumber").value ; 
+    document.getElementById("result").innerHTML = num1 * num2;
+
+}
+
+function devide(){
+    num1 = document.getElementById("firstNumber").value ; 
+    num2 = document.getElementById("secondNumber").value ; 
+    document.getElementById("result").innerHTML = num1 / num2;
+
+}
